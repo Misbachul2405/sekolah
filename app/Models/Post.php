865b05adb76ace5d5a9d5cd2a4dc300070e844d3
Misbachul2405 <vicki240593@gmail.com>
2022-10-21 18:52:35 +0700

    penambahan function eloquent relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Post extends Model
 {
@@ -17,6 +20,27 @@ class Post extends Model
     protected $guarded = [];
 
     /**
+     * categori
+     * 
+     * @return void
+     */
+
+     public function category()
+     {
+        return $this->belongsTo(Category::class);
+     }
+
+     /**
+      * tags
+      *
+      *@return void
+      */
+      public function tags()
+      {
+        return $this->belongsToMany(Tag::class);
+      }
+
+    /**
      * image
      *
      * @return Attribute
@@ -25,6 +49,19 @@ class Post extends Model
     {
         return Attribute::make(
             get: fn ($value) => asset('/storage/categories/' . $value),
+        );
+    }
+
+    /**
+     * getCreatedAtAttribute
+     * 
+     * @param mixed $date
+     * @return void
+     */
+    protected function createAt(): Attribute 
+    {
+        return Attribute::make(
+            get: fn($value) => Carbon::parse($value)->format('d-M-Y'),
         );
     }
 }
