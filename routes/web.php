1fe5412faa0525a745f5ff393f89d9d25854ee78
Misbachul2405 <vicki240593@gmail.com>
2022-10-24 13:15:36 +0700

    dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::prefix('admin')->group(function () {
+
+    Route::group(['middleware' => 'auth'], function(){
+
+        //dashboard
+        Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin.dashboard.index');
+
+    });
+
+});
